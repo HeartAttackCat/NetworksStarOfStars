97f7ffff0782c0cadda724f03ab8d3d7ef646cf8
HeartAttackCat <hunter.lane@student.nmt.edu>
2024-11-20 21:29:00 -0700

    Got node to send frame.

--- app/src/main/java/io/hunter/node/Node.java
+++ app/src/main/java/io/hunter/node/Node.java
@@ -1,11 +1,20 @@
 package io.hunter.node;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import io.hunter.model.FrameLibrary;
+import io.hunter.model.NetworkFrame;
+
 public class Node implements Runnable{
 
     public NodeConfig config;
 
     public byte network = 1;
     public byte name = 1;
+
+    private Socket socket;
 
     public Node(byte network, byte name) {
         this.network = network;
@@ -20,6 +29,32 @@ public class Node implements Runnable{
     }
 
     public void run() {
-        
+        connect();
+        try {
+            BufferedOutputStream writer = new BufferedOutputStream(socket.getOutputStream());
+            while(true) {
+                NetworkFrame frame = config.getFrame();
+                if(frame == null)
+                    break;
+
+                Thread.sleep(1000);
+                writer.write(frame.generateFrame());
+                writer.flush();
+                frame.debugFrame();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket("localhost", 25565);
+        } catch (IOException e) {
+            System.out.println("[Node "+name+" Network "+network+"] Did not connect u_m_u");
+        }
     }
 }
