56725c8093160e4cff7e91c533d5ab70c8c85b33
HeartAttackCat <hunter.lane@student.nmt.edu>
2024-11-20 21:28:14 -0700

    Setup temporary testing.

--- app/src/main/java/io/hunter/Application.java
+++ app/src/main/java/io/hunter/Application.java
@@ -1,10 +1,20 @@
 package io.hunter;
 
 import io.hunter.node.Node;
+import io.hunter.switchers.SwitchHub;
 
 public class Application {
 
     public static void main(String[] args) {
+        SwitchHub hub = new SwitchHub(3, (byte) 1);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         Node node = new Node((byte) 1, (byte)1);
         node.start();
         Node node2 = new Node((byte) 1, (byte)2);
